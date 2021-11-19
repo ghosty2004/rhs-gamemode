31045793cae65b4c815b946250355bbd9d77f805
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2021-11-19 14:22:22 +0200

    Added map Las Venturas Airport

--- server-package/maps/index.js
+++ server-package/maps/index.js
@@ -6,15 +6,18 @@
 const LS = require("./others/ls");
 const LV = require("./others/lv");
 const Spawn = require("./others/spawn");
+const Voller = require("./others/voller");
 
 module.exports = {
     Load: function() {
         LS.Load();
         LV.Load();
         Spawn.Load();
+        Voller.Load();
     },
     RemoveBuildings: function(player) {
         LS.RemoveBuilding(player);
         LV.RemoveBuilding(player);
+        Voller.RemoveBuilding(player);
     }
 }
