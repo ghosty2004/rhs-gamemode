3651b6da03d513eec3df13df0b558cce06c4ca3c
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2021-11-19 14:47:42 +0200

    Draw distance modify for objects

--- server-package/maps/index.js
+++ server-package/maps/index.js
@@ -4,6 +4,7 @@
 
 /* others */
 const LS = require("./others/ls");
+const LSAir = require("./others/lsair");
 const LV = require("./others/lv");
 const LVAir = require("./others/lvair");
 const Spawn = require("./others/spawn");
@@ -12,6 +13,7 @@ const Voller = require("./others/voller");
 module.exports = {
     Load: function() {
         LS.Load();
+        LSAir.Load();
         LV.Load();
         LVAir.Load();
         Spawn.Load();
@@ -19,6 +21,7 @@ module.exports = {
     },
     RemoveBuildings: function(player) {
         LS.RemoveBuilding(player);
+        LSAir.RemoveBuilding(player);
         LV.RemoveBuilding(player);
         LVAir.RemoveBuilding(player);
         Voller.RemoveBuilding(player);
