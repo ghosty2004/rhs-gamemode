762a54ea042b9932c021b05a321982eea1c159d6
Ghosty2004 <72819231+Ghosty2004@users.noreply.github.com>
2021-11-20 12:21:03 +0200

    Teleport module typescript changes

--- server-package/modules/teleport/index.d.ts
+++ server-package/modules/teleport/index.d.ts
@@ -1,9 +1,9 @@
 export declare const Info: {
     id: number,
     name: string,
-    position: Array<string>
+    position: Array<number>
 }[];
-export declare function Create(id: number, commandname: string, name: string, position: Array<string>): boolean;
+export declare function Create(id: number, commandname: string, name: string, position: Array<number>): boolean;
 export declare function Delete(commandname: string): boolean;
 export declare function Exists(commandname: string): boolean;
 export declare function ExistsId(id: number): boolean;
