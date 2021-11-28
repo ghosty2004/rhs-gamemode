b3c9501c0fdb1ebbd776e16ab04a11033708a567
Ghosty2004 <fejerszilveszter@gmail.com>
2021-11-28 19:52:33 +0000

    Added command /slapall ;

--- server_package/index.js
+++ server_package/index.js
@@ -2486,9 +2486,15 @@ CMD.on("unfreezeall", (player) => {
 
 CMD.on("slapall", (player) => {
     if(Player.Info[player.playerid].Admin < 3) return SendError(player, Errors.NOT_ENOUGH_ADMIN.RO, Errors.NOT_ENOUGH_ADMIN.ENG);
+    samp.getPlayers().filter(f => Player.Info[f.playerid].LoggedIn).forEach((i) => {
+        i.SetPlayerPos(i.position.x, i.position.y, i.position.z+2);
+        i.SetPlayerHealth(i.GetPlayerHealth() - 10);
+        i.SendClientMessage(data.colors.YELLOW, `Admin {FF0000}${player.GetPlayerName(24)} {FFFF00}has slapped all players{FFFF00}!`);
+    });
+    SendACMD(player, "SlapAll");
 });
 
-CMD.on("explodeall", (player, params) => {
+CMD.on("explodeall", (player) => {
     if(Player.Info[player.playerid].Admin < 3) return SendError(player, Errors.NOT_ENOUGH_ADMIN.RO, Errors.NOT_ENOUGH_ADMIN.ENG);
     samp.getPlayers().filter(f => Player.Info[f.playerid].LoggedIn).forEach((i) => {
         samp.CreateExplosion(i.position.x, i.position.y, i.position.z, 7, 5);
