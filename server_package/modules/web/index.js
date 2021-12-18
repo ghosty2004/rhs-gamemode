cef141261bcce6b2181ed7abed05acfdd5f6a851
Ghosty2004 <fejerszilveszter@gmail.com>
2021-12-18 19:52:18 +0200

    New variables in web: "/api/onlineplayers" ;

--- server_package/modules/web/index.js
+++ server_package/modules/web/index.js
@@ -19,13 +19,34 @@ app.get("/api/onlineplayers", (request, response) => {
     let array = [];
     getPlayers().forEach((i) => {
         array.push({
-            id: i.playerid,
-            name: i.GetPlayerName(24),
-            version: i.GetPlayerVersion(24),
-            gpci: i.gpci(41),
-            money: i.GetPlayerMoney(),
-            ping: i.GetPlayerPing(),
-            position: i.position
+            Id: i.playerid,
+            Name: i.GetPlayerName(24),
+            Version: i.GetPlayerVersion(24),
+            Gpci: i.gpci(41),
+            Ping: i.GetPlayerPing(),
+            Position: i.position,
+            Data: {
+                Language: Player.Info[i.playerid].Language == 1 ? "Romana" : "English",
+                AccountID: Player.Info[i.playerid].AccID,
+                Money: Player.Info[i.playerid].Money,
+                Coins: Player.Info[i.playerid].Coins,
+                Respect: Player.Info[i.playerid].Respect,
+                ConnectTime: Player.Info[i.playerid].ConnectTime,
+                OnlineTime: Player.Info[i.playerid].OnlineTime,
+                Admin: Player.Info[i.playerid].Admin,
+                AdminActivity: Player.Info[i.playerid].AdminActivity,
+                RconType: Player.Info[i.playerid].RconType,
+                VIP: Player.Info[i.playerid].VIP,
+                VIP_Expire: Player.Info[i.playerid].VIP_Expire,
+                Clan: Player.Info[i.playerid].Clan,
+                Clan_Rank: Player.Info[i.playerid].Clan_Rank,
+                Gang: Player.Info[i.playerid].Gang,
+                Gang_Data: Player.Info[i.playerid].Gang_Data,
+                Kills_Data: Player.Info[i.playerid].Kills_Data,
+                Driving_Data: Player.Info[i.playerid].Driving_Data,
+                Month: Player.Info[i.playerid].Month,
+                LastOn: Player.Info[i.playerid].LastOn
+            }
         });
     });
     response.json(array);
