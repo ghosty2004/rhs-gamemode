865331639d5f8d3aee4994062592920ce8f68a6c
Ghosty2004 <fejerszilveszter@gmail.com>
2021-12-18 19:54:22 +0200

    Added /api/gangs to web server ;

--- server_package/modules/web/index.js
+++ server_package/modules/web/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { getPlayers } = require("samp-node-lib");
+
+const Gang = require("../gang");
 const Player = require("../player");
 
 /* ============ */
@@ -13,6 +15,10 @@ app.set("json spaces", 4);
 
 app.listen(port, () => {
     console.log(`Web server running on port ${port}`);
+});
+
+app.get("/api/gangs", (request, response) => {
+    response.json(Gang.Info);
 });
 
 app.get("/api/onlineplayers", (request, response) => {
